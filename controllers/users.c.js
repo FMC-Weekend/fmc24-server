274bd6e319fcec773908949a7c7c9e7480089adc
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-10-08 16:53:10 +0530

    routes correction

--- controllers/users.c.js
+++ controllers/users.c.js
@@ -3,35 +3,60 @@ import sqlite3 from 'sqlite3';
 import { v4 as uuidv4 } from 'uuid';
 
 const db = new sqlite3.Database('fmc.sqlite');
-
+const userIndentifier = async (token)=>{
+    const info = await fetch("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + token)
+    // console.log("info", info)
+    const data = await info.json();
+    const name=data.name;
+    const email=data.email;
+    const picture=data.picture;
+    console.log(name,email,picture)
+    return {name,email,picture};
+   
+}
 export const register = async (req, res) => {
     const x = req.body;
-    console.log(req)
-    const { name, email, college, phone, referralCode, instaHandle, type } = req.body;
+    
+    // console.log(req)
+    const { name, email, college, phone, referralCode, instaHandle, type,token } = req.body;
+    
+   const emailVerified=await userIndentifier(token);
+   const em=emailVerified.email;
+   if(em!==email){
+         res.send({message:"email not verified"});
+         return;
+   }
     console.log(name, email, college, phone, referralCode, instaHandle, type)
     db.serialize(() => {
         const stmt = db.prepare("UPDATE users SET Name = ?, Email = ?, College = ?, Phone = ?, ReferralCode = ?, InstaHandle = ?, Type = ? WHERE UserId = ?");
         stmt.run(name, email, college, phone, referralCode, instaHandle, type, userId);
         stmt.finalize();
     });
-    res.send({ body: x });
+    res.send({message:"user updated"});
 };
 export const login = async (req, res) => {
     const { token } = req.body;
-    const info = await fetch("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + token)
-    console.log("info", info)
-    const data = await info.json();
-    const name=data.name;
-    const email=data.email;
-    const picture=data.picture;
+    const {name,email,picture}=await userIndentifier(token);
+    console.log(name,email,picture)
     db.serialize(() => {
-        db.all(`SELECT * FROM users WHERE Email = '${email}' OR Phone = '${phone}'`, (err, rows) => {
+        db.all(`SELECT * FROM users WHERE Email = '${email}'`, (err, rows) => {
             if (err) {
                 console.log(err);
                 res.send({ error: err });
             }
             else {
-                res.send({ rows });
+                //if user does not exist then create a new user
+                if (rows.length == 0) {
+                    const userId = uuidv4();
+                    db.serialize(() => {
+                        const stmt = db.prepare("INSERT INTO users(UserId, Name, Email, Image) VALUES(?,?,?,?)");
+                        stmt.run(userId, name, email, picture);
+                        stmt.finalize();
+                    });
+                    res.send({message:"user created",user:{userId,name,email,picture}});
+                }
+                else
+                res.send({message:"user already exists",user:rows[0]});
             }
         });
     });
