04d69cd0b8c0bf00de8f2a82958ffc4d67d586cb
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-10-08 15:43:07 +0530

    register route

--- controllers/users.c.js
+++ controllers/users.c.js
@@ -4,16 +4,29 @@ import { v4 as uuidv4 } from 'uuid';
 
 const db = new sqlite3.Database('fmc.sqlite');
 
-export const login = async (req, res) => {
-    const x=req.body;
+export const register = async (req, res) => {
+    const x = req.body;
     console.log(req)
-    // const { name, email, college, phone, referralCode, instaHandle, type } = req.body;
-    // db.serialize(() => {
-    //     const stmt = db.prepare("INSERT INTO users(UserId, Name, Email, College, Phone, ReferralCode, InstaHandle, Type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-    //     const userId = uuidv4(); 
-    //     stmt.run(userId, name, email, college, phone, referralCode, instaHandle, type);
-    //     stmt.finalize();
-    // });
-
-    res.send({body:x});
+    const { name, email, college, phone, referralCode, instaHandle, type } = req.body;
+    console.log(name, email, college, phone, referralCode, instaHandle, type)
+    db.serialize(() => {
+        const stmt = db.prepare("UPDATE users SET Name = ?, Email = ?, College = ?, Phone = ?, ReferralCode = ?, InstaHandle = ?, Type = ? WHERE UserId = ?");
+        stmt.run(name, email, college, phone, referralCode, instaHandle, type, userId);
+        stmt.finalize();
+    });
+    res.send({ body: x });
 };
+export const login = async (req, res) => {
+    const { token } = req.body;
+    db.serialize(() => {
+        db.all(`SELECT * FROM users WHERE Email = '${email}' OR Phone = '${phone}'`, (err, rows) => {
+            if (err) {
+                console.log(err);
+                res.send({ error: err });
+            }
+            else {
+                res.send({ rows });
+            }
+        });
+    });
+}
