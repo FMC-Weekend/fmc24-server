60c0d18b4ea1b513e231ff2b1f0c429ebebe13b2
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-10-08 13:35:32 +0530

    create server2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { createServer } from "node:http"
 const app = express();
 const server = createServer(app)
 app.get('/', (req, res) => {
-    res.sendFile(new URL('./index.html', import.meta.url).pathname);
+    res.send("Hello from fmc")
 });
 server.listen(process.env.PORT || 3000, (req, res) => {
     console.log("Server is running on port 3000")
