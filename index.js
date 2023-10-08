b4639cfae2b3bdc31030c30d0d06b93bec79712c
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-10-08 14:37:38 +0530

    login route

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import methodOverride from "method-override";
+
 dotenv.config();
-import { createServer } from "node:http"
+
 const app = express();
 app.use(cors());
-const server = createServer(app)
+
+// Middlewares
+app.set('trust proxy', 1);
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(express.static('.'));
+
+// Routes
 app.get('/', (req, res) => {
-    res.send("Hello from fmc")
+    res.send("Hello from fmc");
 });
-import userRouter from "./routes/users.r.js"
-app.use("/api", userRouter)
-const PORT=process.env.PORT;
-server.listen( PORT ,(req, res) => {
-    console.log(`Server is running on port ${PORT}`)
-})
+
+import userRouter from "./routes/users.r.js";
+app.use("/api", userRouter);
+
+app.post("/login", (req, res) => {
+    res.send({ message: 'Login successful', body: req.body });
+});
+
+const PORT = process.env.PORT || 3000; // Set a default port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
